3899e377a7758c428cf6a386c7d439e592f99ac4
Areeba Saleem <areebasaleem4427@gmail.com>
2024-11-27 20:44:50 +0500

    video changed

--- src/components/Header.js
+++ src/components/Header.js
@@ -21,7 +21,7 @@ function Header({ userData }) {
       <div className="video-section">
         <iframe
           width="100%"
-          src="https://www.youtube.com/embed/kiP9inzMJiE"
+          src="https://www.youtube.com/embed/xAFR2EpeAKI"
           title="Video title"
           frameBorder="0"
           allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
